dba7cc80420609ff31a04c97317af0f530b5f49a
Jakob Persons <jakobep24@gmail.com>
2021-02-04 21:18:19 -0500

    filter needs fixing up

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,17 +1,25 @@
 import React, {useState} from "react";
 
 function CategoryFilter({ allCats, selectedCat, setSelectedCat }) {
+
+  function handleClick(category) {
+    setSelectedCat(category)
+    console.log(category)
+    console.log(selectedCat)
+  }
   
   const catBtns = allCats.map(category => {
     return (
-    <button onClick={() => handleClick(category)} className={selectedCat === category ? "selected" : null} >
+    <button key={category} onClick={() => handleClick(category)} className={selectedCat === category ? "selected" : null} >
       {category}
     </button>)
   })
 
-  function handleClick(category) {
-    setSelectedCat(category)
-  }
+  // function handleClick(category) {
+  //   setSelectedCat(category)
+  //   console.log(category)
+  //   console.log(selectedCat)
+  // }
 
   return (
     <div className="categories">
